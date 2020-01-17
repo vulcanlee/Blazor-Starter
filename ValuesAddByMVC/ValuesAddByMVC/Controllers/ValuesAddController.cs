e99624e9b10b5635028383343d29e78aa9bf0bea
李進興 Vulcan <vulcan@miniasp.com>
2020-01-17 21:26:59 +0800

    兩數相加 - 使用 ASP.NET Core MVC

---
+++ ValuesAddByMVC/ValuesAddByMVC/Controllers/ValuesAddController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ValuesAddByMVC.Controllers
+{
+    public class ValuesAddController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult add()
+        {
+            int num1 = Convert.ToInt32(HttpContext.Request.Form["value1"].ToString());
+            int num2 = Convert.ToInt32(HttpContext.Request.Form["value2"].ToString());
+            int result = num1 + num2;
+            ViewBag.SumResult = result.ToString();
+            return View("Index");
+        }
+    }
+}
